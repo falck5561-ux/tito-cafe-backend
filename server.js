86466fac2f707751a7d440c0ebb2dd83980cb7dd
falck5561-ux <falck5561@gmail.com>
2025-10-08 13:13:14 -0600

    Configurar CORS para producción

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Archivo: server.js (Versión Final con todas las rutas)
+// Archivo: server.js (Versión Final con CORS configurado)
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -7,8 +7,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // --- Middlewares ---
-// Este es el orden correcto y crucial.
-app.use(cors());
+
+// --- 1. CONFIGURACIÓN DE CORS (ESTE ES EL CAMBIO) ---
+// Le decimos al backend que solo acepte peticiones de nuestro frontend
+const corsOptions = {
+  origin: 'https://tito-cafe-frontend.onrender.com',
+  optionsSuccessStatus: 200 
+};
+
+app.use(cors(corsOptions)); // <-- USAMOS LA CONFIGURACIÓN AQUÍ
+
 app.use(express.json()); // El "traductor" de JSON
 
 // --- Rutas ---
@@ -18,7 +26,7 @@ app.use('/api/productos', require('./routes/productosRoutes'));
 app.use('/api/ventas', require('./routes/ventasRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/pedidos', require('./routes/pedidosRoutes'));
-app.use('/api/payment', require('./routes/paymentRoutes')); // <-- LÍNEA FINAL AÑADIDA
+app.use('/api/payment', require('./routes/paymentRoutes'));
 
 // Iniciar Servidor
 app.listen(PORT, () => {
