317cde22ab2e6d268bc67c806301d90d52850345
falck5561-ux <falck5561@gmail.com>
2025-10-26 19:39:28 -0600

    chore: forzar deploy en backend

--- middlewares/verificarTienda.js
+++ middlewares/verificarTienda.js
@@ -1,22 +1,22 @@
-// middleWares/verificarTienda.js
+// middleWares/verificarTienda.js (CORREGIDO)
 
 const verificarTienda = (req, res, next) => {
-  // 1. Buscamos el encabezado 'x-tienda-id' en la solicitud
-  const tiendaId = req.headers['x-tienda-id'];
+  // 1. Buscamos el encabezado 'x-tienda-id' en la solicitud
+  const tiendaId = req.headers['x-tienda-id']; // "1" (como string)
 
-  // 2. Si no viene el encabezado, bloqueamos la solicitud
-  if (!tiendaId) {
-    return res.status(400).json({ 
-      error: 'Acceso denegado. Falta el identificador de la tienda (X-Tienda-ID).' 
-    });
-  }
+  // 2. Si no viene el encabezado, bloqueamos la solicitud
+  if (!tiendaId) {
+    return res.status(400).json({ 
+      error: 'Acceso denegado. Falta el identificador de la tienda (X-Tienda-ID).' 
+    });
+  }
 
-  // 3. Si todo está bien, adjuntamos el ID al objeto 'req'
-  //    para que las rutas lo puedan usar más tarde.
-  req.tiendaId = parseInt(tiendaId, 10);
-  
-  // 4. Dejamos que la solicitud continúe
-  next();
+  // --- 3. LÍNEA CORREGIDA ---
+  // Simplemente adjuntamos el ID como string (tal como viene)
+  req.tiendaId = tiendaId;
+  
+  // 4. Dejamos que la solicitud continúe
+  next();
 };
 
 module.exports = verificarTienda;
